ea96a8a335729cee9fe7f5f1cb161986e5460f17
alekssol63 <aleksvs83@yandex.ru>
2018-04-02 20:46:47 +0600

    Update cart.js

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,15 +1,16 @@
 'use strict';
-
 const itemsList = document.querySelector('.items-list');
 function handler(event) {
   const button = event.target;
-  const title = button.getAttribute('data-title');
-  const price = button.getAttribute('data-price');
-  const item = {
-    title : title,
-    price : price
-  };
-  addToCart(item);
+  if (button.classList.contains('add-to-cart')) {
+    const title = button.getAttribute('data-title');
+    const price = button.getAttribute('data-price');
+    const item = {
+      title : title,
+      price : price
+    };
+    addToCart(item);
+  }
 }
 
-itemsList.addEventListener('click', handler)
+itemsList.addEventListener('click', handler);
